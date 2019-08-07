d6768c51f4a511ff4150c97aaaf2c3d9a71b618f
radoslaw-lazur <radoslaw.lazur@gmail.com>
2019-08-07 18:01:10 +0200

    The package calculator with Class Claculator with unit tests was added to module kodilla.testing

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,25 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+
+        Calculator calculator = new Calculator(5, 5);
+
+
+        // addAtoB method unit test
+        if(calculator.addAToB() == 10) {
+            System.out.println("The addAtoB method works properly");
+        } else {
+            System.out.println("The addAtoB method does not work properly");
+        }
+
+        // substractAFromB unit test
+        if(calculator.substractAFromB() == 0) {
+            System.out.println("The substractAFromB method works properly");
+        } else {
+            System.out.println("The substractAFromB method does not work properly");
+        }
     }
 }
