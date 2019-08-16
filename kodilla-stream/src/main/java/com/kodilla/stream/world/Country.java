454bc02fadf81ba17a1832f7c6254b83ca407622
radoslaw-lazur <radoslaw.lazur@gmail.com>
2019-08-16 18:08:53 +0200

    Kodilla Module 7.4 Total People Quantity. Task in package com.kodilla.stream.world in main and test

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/world/Country.java
@@ -0,0 +1,18 @@
+package com.kodilla.stream.world;
+
+import java.math.BigDecimal;
+
+public final class Country {
+    private final String country;
+    private final BigDecimal peopleQuantity;
+
+    public Country(final String country, final BigDecimal peopleQuantity) {
+        this.country = country;
+        this.peopleQuantity = peopleQuantity;
+
+    }
+
+    public BigDecimal getPeopleQuantity() {
+        return peopleQuantity;
+    }
+}
