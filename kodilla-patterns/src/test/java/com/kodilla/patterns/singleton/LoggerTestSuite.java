92b706845e8dc1d93dcf3fb6409ed441d3bf8a74
radoslaw-lazur <radoslaw.lazur@gmail.com>
2019-10-11 20:24:33 +0200

    Logger Kodilla 15.1

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -0,0 +1,17 @@
+package com.kodilla.patterns.singleton;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LoggerTestSuite {
+
+    @Test
+    public void testGetLastLog() {
+        //Given
+        Logger.getInstance().log("log");
+        //When
+        String result = Logger.getInstance().getLastLog();
+        //Then
+        Assert.assertEquals(result, "log");
+    }
+}
