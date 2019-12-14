d63086d456a8a6da97dee8463ca6fb58d1a07ae0
radoslaw-lazur <radoslaw.lazur@gmail.com>
2019-12-14 16:55:44 +0100

    Kodilla 24.1 Focade

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/dao/EmployeeDao.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/dao/EmployeeDao.java
@@ -14,4 +14,6 @@ import java.util.List;
 public interface EmployeeDao extends CrudRepository<Employee, Integer> {
     @Query
     List<Employee> retrieveEmployeeByLastName(@Param("LASTNAME") String lastName);
+    @Query
+    List<Employee> retrieveEmployeeByRandomCharString(@Param("LASTNAME") String randomCharString);
 }
---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/facade/FocadeTestSuite.java
@@ -0,0 +1,94 @@
+package com.kodilla.hibernate.manytomany.facade;
+
+import com.kodilla.hibernate.manytomany.Company;
+import com.kodilla.hibernate.manytomany.Employee;
+import com.kodilla.hibernate.manytomany.dao.CompanyDao;
+import com.kodilla.hibernate.manytomany.dao.EmployeeDao;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+import java.util.Optional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FocadeTestSuite {
+    @Autowired
+    CompanyDao companyDao;
+    @Autowired
+    EmployeeDao employeeDao;
+    @Autowired
+    ServiceFocade serviceFocade;
+
+    private int softwareMachineId;
+    private int dataMaestersId;
+    private int greyMatterId;
+
+    @Before
+    public void before() {
+        Employee johnSmith = new Employee("John", "Smith");
+        Employee stephanieClarckson = new Employee("Stephanie", "Clarckson");
+        Employee lindaKovalsky = new Employee("Linda", "Kovalsky");
+
+        Company softwareMachine = new Company("Software Machine");
+        Company dataMaesters = new Company("Data Maesters");
+        Company greyMatter = new Company("Grey Matter");
+
+        softwareMachine.getEmployees().add(johnSmith);
+        dataMaesters.getEmployees().add(stephanieClarckson);
+        dataMaesters.getEmployees().add(lindaKovalsky);
+        greyMatter.getEmployees().add(johnSmith);
+        greyMatter.getEmployees().add(lindaKovalsky);
+
+        johnSmith.getCompanies().add(softwareMachine);
+        johnSmith.getCompanies().add(greyMatter);
+        stephanieClarckson.getCompanies().add(dataMaesters);
+        lindaKovalsky.getCompanies().add(dataMaesters);
+        lindaKovalsky.getCompanies().add(greyMatter);
+
+        companyDao.save(softwareMachine);
+        softwareMachineId = softwareMachine.getId();
+        companyDao.save(dataMaesters);
+        dataMaestersId = dataMaesters.getId();
+        companyDao.save(greyMatter);
+        greyMatterId = greyMatter.getId();
+    }
+
+    @After
+    public void cleanUp() {
+        try {
+            employeeDao.deleteAll();
+            companyDao.deleteAll();
+        } catch (Exception e) {
+           //stuff
+        }
+    }
+
+    @Test
+    public void testRandomCharStringCompany() {
+        //When
+        Optional<List<Company>> optionalCompanyList = Optional.ofNullable(companyDao.retrieveCompanyByRandomCharString("Mach"));
+        //Then
+        Assert.assertTrue(optionalCompanyList.isPresent());
+        List<Company> companyListFromDb = optionalCompanyList.get();
+        Assert.assertEquals(1, companyListFromDb.size());
+        Assert.assertEquals("Software Machine", companyListFromDb.get(0).getName());
+    }
+
+    @Test
+    public void testRandomCharStringEmployee() {
+        //When
+        Optional<List<Employee>> optionalEmployeeList = Optional.ofNullable(employeeDao.retrieveEmployeeByRandomCharString("Kov"));
+        //Then
+        Assert.assertTrue(optionalEmployeeList.isPresent());
+        List<Employee> employeeListFromDb = optionalEmployeeList.get();
+        Assert.assertEquals(1, employeeListFromDb.size());
+        Assert.assertEquals("Kovalsky", employeeListFromDb.get(0).getLastname());
+    }
+}
