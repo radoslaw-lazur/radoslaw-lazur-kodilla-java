139ff0dc0f3076a30c371ace81581764852d5ad9
radoslaw-lazur <radoslaw.lazur@gmail.com>
2019-12-16 17:40:02 +0100

    24-5 libraryab to adapt - update Book to BookA

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/bookclassifier/MedianAdapter.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/bookclassifier/MedianAdapter.java
@@ -1,6 +1,6 @@
 package com.kodilla.patterns2.adapter.bookclassifier;
 
-import com.kodilla.patterns2.adapter.bookclassifier.librarya.Book;
+import com.kodilla.patterns2.adapter.bookclassifier.librarya.BookA;
 import com.kodilla.patterns2.adapter.bookclassifier.librarya.Classifier;
 import com.kodilla.patterns2.adapter.bookclassifier.libraryb.BookB;
 import com.kodilla.patterns2.adapter.bookclassifier.libraryb.BookSignature;
@@ -12,11 +12,11 @@ import java.util.Set;
 
 public class MedianAdapter extends MedianAdaptee implements Classifier {
     @Override
-    public double publicationYearMedian(Set<Book> bookSet) {
+    public double publicationYearMedian(Set<BookA> bookASet) {
         Map<BookSignature, BookB> bookMap = new HashMap<>();
-        for (Book book : bookSet) {
-            BookB bookB = new BookB(book.getAuthor(), book.getTitle(), book.getPublicationYear());
-            bookMap.put(new BookSignature(book.getSignature(), bookB), bookB);
+        for (BookA bookA : bookASet) {
+            BookB bookB = new BookB(bookA.getAuthor(), bookA.getTitle(), bookA.getPublicationYear());
+            bookMap.put(new BookSignature(bookA.getSignature(), bookB), bookB);
         }
         return medianPublicationYear(bookMap);
     }
--- kodilla-patterns2/src/test/java/com/kodilla/patterns2/adapter/bookclassifier/MedianAdapterTestSuite.java
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/adapter/bookclassifier/MedianAdapterTestSuite.java
@@ -1,6 +1,6 @@
 package com.kodilla.patterns2.adapter.bookclassifier;
 
-import com.kodilla.patterns2.adapter.bookclassifier.librarya.Book;
+import com.kodilla.patterns2.adapter.bookclassifier.librarya.BookA;
 import org.junit.Assert;
 import org.junit.Test;
 import java.util.HashSet;
@@ -11,14 +11,14 @@ public class MedianAdapterTestSuite {
     @Test
     public void publicationYearMedianTest() {
         //Given
-        Set<Book> bookSet = new HashSet<>();
-        bookSet.add(new Book("Author1", "Title1", 2000, "Signature1"));
-        bookSet.add(new Book("Author2", "Title2", 1989, "Signature2"));
-        bookSet.add(new Book("Author3", "Title3", 2005, "Signature3"));
-        bookSet.add(new Book("Author4", "Title4", 2003, "Signature4"));
+        Set<BookA> bookASet = new HashSet<>();
+        bookASet.add(new BookA("Author1", "Title1", 2000, "Signature1"));
+        bookASet.add(new BookA("Author2", "Title2", 1989, "Signature2"));
+        bookASet.add(new BookA("Author3", "Title3", 2005, "Signature3"));
+        bookASet.add(new BookA("Author4", "Title4", 2003, "Signature4"));
         MedianAdapter medianAdapter = new MedianAdapter();
         //When
-        double median = medianAdapter.publicationYearMedian(bookSet);
+        double median = medianAdapter.publicationYearMedian(bookASet);
         System.out.println(median);
         //Then
         Assert.assertEquals(2003.0, median, 0);
