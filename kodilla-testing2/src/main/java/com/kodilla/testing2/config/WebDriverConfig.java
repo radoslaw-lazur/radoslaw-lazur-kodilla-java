4559da2885c877e5bdf5d0e428c6ba8c2f26a5bf
radoslaw-lazur <radoslaw.lazur@gmail.com>
2020-02-08 20:08:59 +0100

    31-4 deleted import

--- kodilla-testing2/src/main/java/com/kodilla/testing2/config/WebDriverConfig.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/config/WebDriverConfig.java
@@ -2,7 +2,6 @@ package com.kodilla.testing2.config;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class WebDriverConfig {
